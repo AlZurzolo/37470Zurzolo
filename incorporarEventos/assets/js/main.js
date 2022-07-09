448eb61c4ea2a5a4c284bb6562817656910537ca
Zurolo Aldo Enrique <103467835+AlZurzolo@users.noreply.github.com>
2022-07-09 20:27:53 -0300

    Incorporar eventos

--- incorporarEventos/assets/js/main.js
+++ incorporarEventos/assets/js/main.js
@@ -16,7 +16,7 @@ class Carrito {
     calcularTotal() {
         let total = 0;
         for(let i = 0; i < this.productos.length; i++) {
-            total = total + this.productos[i].precio;
+            total += this.productos[i].precio;
         }
         return total;
     }
@@ -27,17 +27,37 @@ class Carrito {
 
 function renderCard(producto) {
     let cardRendered = `    
-    <div class="card m-6" style="width: 20rem;">
-        <img src="./../assets/img/${producto.imagen}" class="card-img-top" alt="...">
-        <div class="card-body">
-            <h5 class="card-title">${producto.id}.${producto.nombre}</h5>
-            <!-- <p class="card-text">$${producto.precio}</p> -->
-            <!-- <a href="#" class="btn btn-primary botonDeCompra" id="${producto.id}">Agregar al carrito</a> -->
+    <div class="col-12 col-md-4">
+        <div class="card" style="width: 18rem;">
+            <img src="./assets/img/${producto.imagen}" class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="card-title">${producto.id}. ${producto.nombre}</h5>
+                <h5 class="card-text">$${producto.precio}</h5>
+                <a href="#" class="btn btn-primary botonDeCompra" id="${producto.id}">Agregar al carrito</a>
+            </div>
         </div>
     </div>
     `;
     return cardRendered;
 }
+
+function renderCardA(producto) {
+    let cardRendered = `    
+    <div class="col-md-4">
+         <div class="card" style="width: 18rem;">
+             <img src="./assets/img/${producto.imagen}"  width="100px">
+                 <div class="card-body">
+                 <h5 class="card-title">${producto.nombre}</h5>
+                 <h5 class="card-text">$${producto.precio}</h5>
+                 <!-- <button onclick="eliminarDelCarrito(${producto.id})" class="btn btn-danger">X</button> -->
+             </div>
+         </div>
+     </div>
+     `;
+
+    return cardRendered;
+}
+
 
 function limpiarCarrito() {
     let divCarrito = document.querySelector("#carrito");
@@ -47,9 +67,9 @@ function limpiarCarrito() {
 function actualizarCarrito(carrito) {
     let divCarrito = document.querySelector("#carrito");
     carrito.productos.forEach(producto => {
-        divCarrito.innerHTML += renderCard(producto);
+    divCarrito.innerHTML += renderCardA(producto);
     })
-    divCarrito.innerHTML += `<h1>Precio Total: $ ${carrito.calcularTotal()}</h1>`
+    divCarrito.innerHTML += `<h2>Precio Total: $${carrito.calcularTotal()}</h2>`
 }
 
 function renovarStorage() {
@@ -68,13 +88,17 @@ window.addEventListener('DOMContentLoaded', (e) => {
     actualizarCarrito(carritoGuardado);
 });
 
+
 /* Generación de mi catálogo de productos */
 let catalogoProductos = [];
+
 let producto1 = new Producto(1, "Transgeneracional", "transgeneracional.png", 15000);
 let producto2 = new Producto(2, "Protección Energética", "proteccion_energetica.jpg", 3000);
-let producto3 = new Producto(3, "Retiro Sanando el Alma", "retiro_virtual.png", 6000);
+let producto3 = new Producto(3, "Retiro 'Sanando el Alma'", "retiro_virtual.png", 6000);
 let producto4 = new Producto(4, "Transformación Personal", "transformacion-personal.png", 6000);
 let producto5 = new Producto(5, "Sanación de 7 Días", "sanacion7dias.png", 1000);
+
+/* Carga de productos al catálogo */
 catalogoProductos.push(producto1);
 catalogoProductos.push(producto2);
 catalogoProductos.push(producto3);
---
+++ pruebas/main.js
@@ -0,0 +1,6 @@
+function sumar(primerNumero, segundoNumero) {
+    return primerNumero+segundoNumero;
+    }
+    let resultado = sumar(5, 8);
+
+    console.log (resultado);
